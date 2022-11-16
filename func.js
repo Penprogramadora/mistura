40ea1af87351c45bf25ce158b2da8356de619ecf
Penprogramadora <103970208+Penprogramadora@users.noreply.github.com>
2022-11-16 20:47:50 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,18 @@
+WebCam.attach("#camera");
+camera=document.getElementById("camera");
+Webcam.set({ width:350, height:300, image_format : 'png', png_quality:90 });
+function take_snapshot() 
+{ Webcam.snap(function(data_uri) { document.getElementById("result").innerHTML =
+ '<img id="selfie_image" src="'+data_uri+'"/>';
+ }); }
+ console.log("ml5 version: ", ml5.vesion);
+ classifier = ml5.imageClassifier ("https://teachablemachine.withgoogle.com/models/fgbKoP7B0/");
+ function modelLoaded() { console.log('Model Loaded!'); } function check() { img = document.getElementById('selfie_image');
+classifier.classify(img, gotResult); }
+  function gotResult(error, results) 
+    if (error)  
+    { console.error(error); }
+     else 
+     
+     console.log(results); document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); } }
